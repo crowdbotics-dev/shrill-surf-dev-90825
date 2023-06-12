cda357e2ea263b0251063a5f92bedb1ff58fe1b8
Crowdbotics <engineering@crowdbotics.com>
2023-06-12 14:33:21 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/shrill_surf_dev_90825/MainActivity.java
@@ -0,0 +1,15 @@
+package com.shrill_surf_dev_90825;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "shrill_surf_dev_90825";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shrill-surf-dev-90825",
+  url: "https://shrill-surf-dev-90825.botics.co",
+  api: "https://shrill-surf-dev-90825.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
